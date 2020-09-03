50e3bbf5e0bfa82bcb5c116c736465d176e5ad08
Pawel <pawelrugor26@wp.pl>
2020-09-03 22:58:21 +0200

    Log file not exists handling

--- index.js
+++ index.js
@@ -14,33 +14,18 @@ function currentDateFormatted() {
     return [day, month, year].join('-');
 }
 
-function fileExists(filepath) {
-    fs.access(filepath, fs.F_OK, (err) => {
-        if (err) {
-            return false;
-        }
-        return true;
-    })
-}
-
 function addLog(filepath, log) {
-    if (!fileExists(filepath)) {
-        fs.writeFile(filename, log, function (err) {
-            if (err) {
-                //todo: handle error somehow 
-                throw err;
-            }
-        });
-    } else {
-        fs.appendFile(filename, log, function (err) {
-            if (err) {
-                //todo: handle error somehow
-                throw err;
-            }
-        });
-    }
+    fs.appendFile(filepath, log, function (err) {
+        if (err) {
+            fs.writeFile(filepath, log, function (err) {
+                if (err) {
+                    //todo: handle error somehow 
+                    throw err;
+                }
+            });
+        }
+    });
 }
-
 
 function logHandler(settings) {
     //todo: change user if it change, and it will
